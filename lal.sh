23bcd218bc40ae266fa4e49a690e2dd75675c058
arninovi02 <85423922+arninovi02@users.noreply.github.com>
2021-06-08 09:25:45 +0700

    Update lal.sh

--- lal.sh
+++ lal.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-#################################
-
 POOL=eth-us-east1.nanopool.org:9999
 WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.lalian
-
-#################################
-##  End of user-editable part  ##
-#################################
 
 cd "$(dirname "$0")"
 
