8d0d25118ffb00f6ca547706b1f1af57fcbf79af
arninovi02 <85423922+arninovi02@users.noreply.github.com>
2021-06-10 10:13:40 +0700

    Update lal.sh

--- lal.sh
+++ lal.sh
@@ -4,4 +4,4 @@ WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$(echo "$(curl -s ifconfig.me)
 
 cd "$(dirname "$0")"
 
-chmod +x ./lalian && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
+chmod +x ./lalian && ./lalian --algo ETCHASH --pool $POOL --user $WALLET $@
